1c99738cd2570f21b01c086433f7a10f7df5b9bc
Alperen Şanlı <alperensanli.46@gmail.com>
2023-08-25 20:28:37 +0300

    master commit 2

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,5 +1,7 @@
 package Utility;
 
+
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,7 +21,7 @@ public class BaseDriver {
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.get("https://www.akakce.com");
+
 
     }
     public static void WaitAndClose(){
